5128612a54c2909e1150d81d93813d9134a93c77
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-05-31 02:41:47 -0700

    removed modules outside of front abnd backend folder

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user', 
-    required: true
+    ref: "user",
+    required: true,
   },
   text: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const Message = mongoose.model('message', messageSchema);
+const message = mongoose.model("message", messageSchema);
 
 module.exports = message;
